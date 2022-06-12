3a858e2418a388b36ac25e1799b6aff706250e42
Chris <Chris@host.docker.internal>
2022-06-11 21:58:44 -0500

    Agregando mi parte del proyecto

--- CV_GIT/src/cv_git/Gaibor.java
+++ CV_GIT/src/cv_git/Gaibor.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cv_git;
 
-/**
- *
- * @author JC
- */
 public class Gaibor {
+    
+    public void Saludar(){
+        System.out.println("epa");
+    }
     
 }
